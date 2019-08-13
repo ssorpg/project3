24ce1f97283f96e5a8b72dd64cf74c68525ead31
Matt <mattelliottus@gmail.com>
2019-08-13 13:59:53 -0700

    page and component for feeds

--- client/src/components/pages/feed.js
+++ client/src/components/pages/feed.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import Card from '../card.js';
 
 export default function Feed(props) {
+  let comments = [1,2,3,4,5,6,7,8,9,10,11,12];
+
   return (
     <Container>
       <Row>
         <Col>
           <h1>The Network Feed</h1>
         </Col>
+      </Row>
+      <Row>
+        <Col className="col-12">
+          <form class="form-group">
+            <input type="text" name="feed-comment" placeholder="Tell the community what you're thinking…" />
+            <button type="submit" value="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </Col>
+      </Row>
+      <Row>
+        {comments.map( () => {
+          return (
+            <Col className="col-xs-12 col-md-6">
+              <div className="comment">
+                <Card cardClass={ "text-dark text-left card"}>
+              <h4 class="username">Father Jackson</h4>
+              <Row>
+              <Col className="col-3">
+                  <figure className="float:right"
+                    style={{
+                      borderRadius: '150px',
+                      overflow: 'hidden'
+                    }}>
+                    <img src="http://place-hold.it/150" alt="Picture of a guy" style={{maxWidth: '100%'}} />
+                  </figure>
+                </Col>
+                <Col className="col-9">
+                  <p className="comment">Man, this site is great! I love keeping in touch with my family without having to deal with a large corporation!</p>
+                  <ul style={{padding: 0}}>
+                    <li class="btn btn-like" style={{paddingLeft: 0}}>
+                      <a href="#">Like</a>
+                    </li>
+                    <li class="btn btn-dislike">
+                      <a href="#">Dislike</a>
+                    </li>
+                    <li class="btn btn-comment">
+                      <a href="#">Comment</a>
+                    </li>
+                  </ul>
+                </Col>
+              </Row>
+            </Card>
+              </div>
+            </Col>  
+          );
+        })}
       </Row>
     </Container>
   );
