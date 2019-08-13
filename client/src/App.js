dd5c9604a1cd5935068dc5270e5b7e3ec0476313
Matt <mattelliottus@gmail.com>
2019-08-13 12:40:19 -0700

    made components for profile page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/navbar.js';
+import Profile from './components/pages/profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Nav />
       <h1>MATT KEV AND JON ARE COOL</h1>
+      <Profile />
     </div>
   );
 }
