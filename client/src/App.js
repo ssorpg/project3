8d8302e4b0f074cb273d1dcfa82012b10f81b114
kevdoan <kevdoan91@gmail.com>
2019-08-13 12:54:50 -0700

    register page and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Nav from './components/navbar';
-import Container from './components/container'
-import { LoginForm } from './components/form';
-import { LoginButton, Register } from './components/buttons';
-import { Footer } from './components/footer';
 import Login from './pages/login';
+import RegisterPage from './pages/register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-
-    <div className="App">
-      <Login />
-    </div>
-  );
+class TPN extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/register" component={RegisterPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  };
 }
 
-export default App;
+export default TPN;
