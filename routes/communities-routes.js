c2494bf6a4456d2d9c19e5cd9694fc7c9cd75351
Jonathan Baker <ssorpg@gmail.com>
2019-08-14 04:50:40 -0700

    finish wrapping routes for error handling

--- routes/communities-routes.js
+++ routes/communities-routes.js
@@ -2,48 +2,49 @@ const db = require("../models");
 
 module.exports = function(app) {
     const route = '/api/communities';
+    const wrap = fn => (...args) => fn(...args).catch(args[2]); // async error handling
 
-  app.post(route, function(req, res) { // register community
+  app.post(route, wrap(async function (req, res, next) { // register community
     
-  });
+  }));
 
-  app.post(route, function(req, res) { // get all communities?
+  app.post(route, wrap(async function (req, res, next) { // get all communities?
 
-  });
+  }));
 
-  app.get(route + '/:commID', function(req, res) { // get community info
+  app.get(route + '/:commID', wrap(async function (req, res, next) { // get community info
 
-  });
+  }));
 
-  app.delete(route + '/:commID', function(req, res) { // delete community
+  app.delete(route + '/:commID', wrap(async function (req, res, next) { // delete community
 
-  });
+  }));
 
-  app.post(route + ':commID/users/:userID', function(req, res) { // join community
+  app.post(route + ':commID/users/:userID', wrap(async function (req, res, next) { // join community
 
-  });
+  }));
 
-  app.delete(route + '/:commID/users/:userID', function(req, res) { // leave community
+  app.delete(route + '/:commID/users/:userID', wrap(async function (req, res, next) { // leave community
 
-  });
+  }));
 
-  app.get(route + '/:commID/events', function(req, res) { // community events
+  app.get(route + '/:commID/events', wrap(async function (req, res, next) { // community events
 
-  });
+  }));
 
-  app.post(route + '/:commID/events', function(req, res) { // create event
+  app.post(route + '/:commID/events', wrap(async function (req, res, next) { // create event
 
-  });
+  }));
 
-  app.get(route + '/:commID/events/:eventID', function(req, res) { // create event
+  app.get(route + '/:commID/events/:eventID', wrap(async function (req, res, next) { // create event
 
-  });
+  }));
 
-  app.delete(route + '/:commID/events/:eventID', function(req, res) { // delete event
+  app.delete(route + '/:commID/events/:eventID', wrap(async function (req, res, next) { // delete event
 
-  });
+  }));
 
-  app.put(route + '/:commID/events/:eventID', function(req, res) { // edit event
+  app.put(route + '/:commID/events/:eventID', wrap(async function (req, res, next) { // edit event
 
-  });
+  }));
 };
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -1,41 +1,42 @@
 const db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   const route = '/api/posts';
+  const wrap = fn => (...args) => fn(...args).catch(args[2]); // async error handling
 
-  app.get(route, function(req, res) {   // get all posts of type implied by provided queryID(s)
+  app.get(route, wrap(async function (req, res, next) {   // get all posts of type implied by provided queryID(s)
     const commID = req.query.commID;    // all posts have commID, but if only has commID it's a feed post,
     const userID = req.query.userID;    // if has userID it's a wall post,
     const eventID = req.query.eventID;  // else if has eventID it's an event post
-  });
+  }));
 
-  app.post(route, function(req, res) { // make post of type implied by provided queryID(s)
+  app.post(route, wrap(async function (req, res, next) { // make post of type implied by provided queryID(s)
     const commID = req.query.commID;
     const userID = req.query.userID;
     const eventID = req.query.eventID;
-  });
+  }));
 
-  app.delete(route + '/:postID', function(req, res) {
+  app.delete(route + '/:postID', wrap(async function (req, res, next) {
 
-  });
+  }));
 
-  app.put(route + '/:postID', function(req, res) { // edit post
+  app.put(route + '/:postID', wrap(async function (req, res, next) { // edit post
 
-  });
+  }));
 
-  app.get(route + '/:postID/comments', function(req, res) { // get comments on post
+  app.get(route + '/:postID/comments', wrap(async function (req, res, next) { // get comments on post
 
-  });
+  }));
 
-  app.post(route + '/:postID/comments', function(req, res) { // make comment on post
+  app.post(route + '/:postID/comments', wrap(async function (req, res, next) { // make comment on post
 
-  });
+  }));
 
-  app.delete(route + '/:postID/comments/:commeID', function(req, res) {
+  app.delete(route + '/:postID/comments/:commeID', wrap(async function (req, res, next) {
 
-  });
+  }));
 
-  app.put(route + '/:postID/comments/:commeID', function(req, res) { // edit comment
+  app.put(route + '/:postID/comments/:commeID', wrap(async function (req, res, next) { // edit comment
 
-});
+  }));
 };
