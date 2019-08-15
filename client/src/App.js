720b043131e27de33562f0eccd4be7a1706f7618
kevdoan <kevdoan91@gmail.com>
2019-08-15 14:33:02 -0700

    small fixes, chat app button, ws instead of socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+//import './App.css';
 import Nav from './components/navbar.js';
 import Profile from './components/pages/profile';
-import { Footer } from './components/footer';
+import  Footer  from './components/footer';
 import Login from './components/pages/login';
 import RegisterPage from './components/pages/register';
 import Feed from './components/pages/feed';
 import Friends from './components/pages/friends';
+import Chat from './components/pages/chat';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/styles.css';
 
@@ -24,6 +25,7 @@ class TPN extends Component {
             <Route path="/feed" component={Feed} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/friends" component={Friends} />
+            <Route exact path="/chat" component={Chat} />
           </Switch>
         </div>
       </Router>
