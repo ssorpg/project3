6889aa511dd32ff0239758baeb3564a870f145d5
kevdoan <kevdoan91@gmail.com>
2019-08-17 23:05:17 -0700

    new image form... new save path..

--- client/src/components/imageupload.js
+++ client/src/components/imageupload.js
@@ -4,43 +4,93 @@ import axios from 'axios';
 
 export default class ImageUpload extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { pictures: [] };
-        this.onDrop = this.onDrop.bind(this);
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { pictures: [] };
+    //     this.onDrop = this.onDrop.bind(this);
+    // }
 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        }, () => {
-            let fd = new FormData();
-            console.log(this.state.pictures);
-            fd.append('image', this.state.pictures[0]);
-            //console.log(fd);
+    // onDrop(picture) {
+    //     this.setState({
+    //         pictures: this.state.pictures.concat(picture),
+    //     }, () => {
+    //         let fd = new FormData();
+    //         console.log(this.state.pictures[0]);
+    //         fd.append('file', this.state.pictures[0]);
+    //         //console.log(fd);
 
-            axios.post(`/api/images`, {
-                type: 'post',
-                data: fd,
-                contentType: false,
-                processData: false,
-            }).then(res => {
-                console.log(res);
-            });
-        });
-    }
+    //         axios.post(`/api/images`, {
+    //             type: 'post',
+    //             data: fd,
+    //             contentType: false,
+    //             processData: false,
+    //         }).then(res => {
+    //             console.log(res);
+    //         });
+    //     });
+    // }
 
 
 
-    render() {
+    // render() {
+    //     return (
+    //         <ImageUploader
+    //             withIcon={true}
+    //             buttonText='Choose images'
+    //             onChange={this.onDrop}
+    //             imgExtension={['.jpg', '.gif', '.png', '.gif']}
+    //             maxFileSize={5242880000000}
+    //         />
+    //     );
+    constructor() {
+        super();
+        this.state = {
+          description: '',
+          selectedFile: '',
+        };
+      }
+
+      onChange = (e) => {
+        switch (e.target.name) {
+          case 'selectedFile':
+            this.setState({ selectedFile: e.target.files[0] });
+            break;
+          default:
+            this.setState({ [e.target.name]: e.target.value });
+        }
+      }
+
+      onSubmit = (e) => {
+        e.preventDefault();
+        const { description, selectedFile } = this.state;
+        let formData = new FormData();
+
+        formData.append('description', description);
+        formData.append('selectedFile', selectedFile);
+
+        axios.post('/api/images', formData)
+          .then((result) => {
+            console.log(result);
+          });
+      }
+
+      render() {
+        const { description, selectedFile } = this.state;
         return (
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880000000}
+          <form onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={this.onChange}
             />
+            <input
+              type="file"
+              name="selectedFile"
+              onChange={this.onChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
         );
     }
 }
