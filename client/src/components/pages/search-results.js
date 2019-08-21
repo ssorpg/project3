b1ff0a7ac3f5cf7f7280670627676a970f41809d
Matt <mattelliottus@gmail.com>
2019-08-21 10:51:56 -0700

    created search results page and added to routes

---
+++ client/src/components/pages/search-results.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    console.log('search res');
+  }
+
+  render() {
+    return(
+      <h1>SEARCH PAGE!</h1>
+    )
+  }
+}
