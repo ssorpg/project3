dc31199fbc94ec565e04c1ae4050f28d76d2c9c6
Matt Elliott <mattelliottus@gmail.com>
2019-09-02 16:25:21 -0700

    removed log

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -4,7 +4,6 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 module.exports = function(app) {
   app.get('/api/events/', wrap( async function(req, res, next) {
-    console.log('gettin events')
     const events = await db.Event.findAll();
 
     if(events.length === 0) {
