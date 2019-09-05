5f7fb9b29e3e3169406d7c785d93b9f939bb0370
Matt Elliott <mattelliottus@gmail.com>
2019-09-04 17:08:57 -0700

    added route for single event

--- client/src/App.js
+++ client/src/App.js
@@ -81,6 +81,7 @@ export default class TPN extends Component {
               <Route exact path="/community/:CommunityId/friends" render={() => <FriendsController {...this.state} />} />
               <Route exact path="/community/:CommunityId/friends/:UserId" render={() => <Wall {...this.state} />} />
               <Route exact path="/events" render={() => <EventsController {...this.state} />} />
+              <Route exact path="/community/:CommunityId/events/:EventId" render={() => <EventsController {...this.state} />} />
               {/* <Route exact path="/community/:CommunityId/chat" render={() => <Chat {...this.state} />} /> */}
               <Route exact path="/chat" render={() => <ChatController {...this.state} />} />
               <Route path="/search" render={() => <SearchResultsController {...this.state} />} />
