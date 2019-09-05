e9fe5505409005538fd7acc57bfcb5098735bd9e
Matt Elliott <mattelliottus@gmail.com>
2019-09-05 16:03:52 -0700

    added styles, used TextField to materialize the form, moved onChange listener to each input not the whole form, changed radio buttons to check box (using native select)

--- client/src/components/pages/events/makeevent.js
+++ client/src/components/pages/events/makeevent.js
@@ -1,56 +1,109 @@
 // COMPONENTS
-import React from 'react';
-import { Container, Paper } from '@material-ui/core';
-import Modal from '../../modal';
+import React from "react";
+import { Paper } from "@material-ui/core";
+import Modal from "../../modal";
+import { TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStlyes = makeStyles(theme => ({
+  paper: {
+    padding: "25px"
+  },
+  dense: {
+    margin: 5
+  },
+  select: {
+    minWidth: "100%"
+  },
+  button: {
+    marginRight: '15px'
+  }
+}));
 export default function MakeEvent(props) {
+  const classes = useStlyes();
   const { handleInputChange, handleSubmit, alert, communities } = props;
-  //TODO 1 : add google maps and use that to add locations to events
-  //TODO MATERIALIZE
+
+  //TODO 1 : add google maps and use that to add locations to events - youll need the grid
   return (
-    <Container>
-      <Paper>
-        <form
+    <Paper className={classes.paper}>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="name"
+          label="Event Name"
+          placeholder="Placeholder"
+          margin="dense"
+          type="text"
           onChange={handleInputChange}
-          onSubmit={handleSubmit}
+          required
+        />
+
+        <TextField
+          id="description"
+          label="Event Description"
+          placeholder="Placeholder"
+          margin="dense"
+          type="text"
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <TextField
+          id="date"
+          helperText="Event Date"
+          margin="dense"
+          type="date"
+          onChange={handleInputChange}
+          required
+        />
+
+        <TextField
+          id="start_time"
+          helperText="Event Start Time"
+          margin="dense"
+          type="time"
+          onChange={handleInputChange}
+          required
+        />
+
+        <TextField
+          id="end_time"
+          helperText="Event End Time"
+          margin="dense"
+          type="time"
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <TextField
+          id="communityId"
+          className={classes.select}
+          select
+          margin="dense"
+          onChange={handleInputChange}
+          SelectProps={{
+            native: true
+          }}
+          required
         >
-          <label for="name">Event Name</label>
-          <input name="name" type="text" />
-          <br />
-          <label for="description">Event Description</label>
-          <input name="description" type="text" />
-          <br />
-          <label for="date">Event Date</label>
-          <input name="date" type="date" />
-          <br />
-          <label for="start_time">Event Start Time</label>
-          <input name="start_time" type="time" />
-
-          <br />
-          <label for="end_time">Event Start Time</label>
-          <input name="end_time" type="time" />
-          <br /><br />
-          <h5>Select a community for the event:</h5>
-          <ul>
-            {communities.map(item => (
-              <li key={item.id}>
-                <label for='communityId'>
-                  <input type="radio" name='communityId' value={item.id} />
-                  {item.name}
-                </label>
-              </li>
-            ))}
-          </ul>
-
-          <button name="submit" type="submit">Submit</button>
-          <button name="reset" type="reset">Reset</button>
-          {
-            alert ?
-              <Modal error={alert} />
-              : ''
-          }
-        </form>
-      </Paper>
-    </Container>
+          <option selected="selected" disabled="disabled">
+            Choose A Community
+          </option>
+          {communities.map(item => (
+            <option key={item.id} name={item.name} value={item.id}>
+              {item.name}
+            </option>
+          ))}
+        </TextField>
+        <br />
+        <br />
+        <Button className={classes.button} variant="contained" color="primary" name="submit" type="submit">
+          Submit
+        </Button>
+        <Button className={classes.button} variant="outlined" color="secondary" name="reset" type="reset">
+          Reset
+        </Button>
+        {alert ? <Modal error={alert} /> : ""}
+      </form>
+    </Paper>
   );
-};
+}
