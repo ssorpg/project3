ca0652f7daa6e3406b740e898960ed48b2715209
Matt Elliott <mattelliottus@gmail.com>
2019-09-08 13:34:23 -0700

    removed unused geolocation component and removed try/catch around iframe

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,34 +1,27 @@
 import React from "react";
 // FUNCTIONS
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   map: {
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '350px',
-    frameBoarder: '0',
-    border: 'none',
-    marginBotton: '24px'
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "350px",
+    frameBoarder: "0",
+    border: "none",
+    marginBotton: "24px"
   }
 }));
 
 export default function GoogleMap(props) {
   const classes = useStyles();
 
-  // <Map google={props.google} zoom={14} className={classes.map} />
-  // initialCenter={props.location}
-  try {
-    return (
-      <iframe
-        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyASU2wiCjMrhc5-xK1FjDzDyuFI0cyWqx0&q=${props.location}`}
-        allowFullScreen
-        className={classes.map}
-        title="Location Map"
-      ></iframe>
-    );
-  } catch (error) {
-    console.log("err0r", error);
-  }
+  return (
+    <iframe
+      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyASU2wiCjMrhc5-xK1FjDzDyuFI0cyWqx0&q=${props.location}`}
+      allowFullScreen
+      className={classes.map}
+      title="Location Map"
+    ></iframe>
+  );
 }
