d811b71a17e4960dee7bfa4d9c8557cf9726cfa7
Matt Elliott <mattelliottus@gmail.com>
2019-09-08 13:32:40 -0700

    created google map component and added to events

--- client/src/components/pages/events/event.js
+++ client/src/components/pages/events/event.js
@@ -11,6 +11,7 @@ import Megatron from "../../megatron";
 // FUNCTIONS
 import { GetFormattedTime } from '../../../utils/formatTime';
 import ExtractProfileImage from '../../../utils/extractprofileimage';
+import GoogleMap from '../../map';
 
 export default function Event(props) {
   const { YourProfile, thisEvent } = props;
@@ -52,7 +53,7 @@ export default function Event(props) {
         <Grid item xs={12} sm={8}>
           <Box>
             <Typography variant="h6">Location:</Typography>
-            {/* //TODO ADD MAP SHOW EVENT LOCATION AND ADDRESS HERE */}
+            <GoogleMap location={thisEvent.location} />
             {thisEvent.location}
           </Box>
           <Box>
