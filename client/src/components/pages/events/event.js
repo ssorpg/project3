428067654b83c81946fca987aee8d6f736c2a0ba
Matt Elliott <mattelliottus@gmail.com>
2019-09-08 17:16:07 -0700

    cleaned up layout, added attendance nav

--- client/src/components/pages/events/event.js
+++ client/src/components/pages/events/event.js
@@ -1,23 +1,42 @@
 // COMPONENTS
 import React from "react";
 import {
-  Typography, Grid, Divider,
-  Link, List, ListItemAvatar,
-  ListItem, ListItemText, Avatar, Box
+  Typography,
+  Grid,
+  Divider,
+  Link,
+  List,
+  ListItemAvatar,
+  ListItem,
+  ListItemText,
+  Avatar,
+  Box,
+  Button
 } from "@material-ui/core";
-import { AccountCircle } from '@material-ui/icons';
+import {
+  AccountCircle,
+  CheckCircleOutlineOutlined,
+  ExposurePlus1
+} from "@material-ui/icons";
 import Megatron from "../../megatron";
 
 // FUNCTIONS
-import { GetFormattedTime } from '../../../utils/formatTime';
-import ExtractProfileImage from '../../../utils/extractprofileimage';
-import GoogleMap from '../../map';
+import { makeStyles } from "@material-ui/core/styles";
+import { GetFormattedTime } from "../../../utils/formatTime";
+import ExtractProfileImage from "../../../utils/extractprofileimage";
+import GoogleMap from "../../map";
+
+const useStyles = makeStyles(theme => ({
+  attendence: {
+    marginRight: "3px"
+  }
+}));
 
 export default function Event(props) {
-  const { YourProfile, thisEvent } = props;
-
+  const { YourProfile, thisEvent, handleAttendClick } = props;
   const start_time = GetFormattedTime(thisEvent.start_time);
   const end_time = GetFormattedTime(thisEvent.end_time);
+  const classes = useStyles();
 
   return (
     <>
@@ -26,16 +45,19 @@ export default function Event(props) {
         image="/images/event.jpg"
         imagePosition="0 76%"
       />
-      <Grid container className="theme-paddingx2" spacing={1}>
+      <Grid container className="theme-paddingx2" spacing={2}>
         <Grid item xs={6}>
           <Typography variant="body1">
-            <strong>
-              Created By:&nbsp;
-            </strong>
+            <strong>Date: </strong> {thisEvent.date} {start_time} - {end_time}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="body1">
+            <strong>Created By:&nbsp;</strong>
             <Link
               href={
-                YourProfile.id === thisEvent.founder.id ?
-                  '/profile'
+                YourProfile.id === thisEvent.founder.id
+                  ? "/profile"
                   : `/community/${thisEvent.CommunityId}/friends/${thisEvent.founder.id}`
               }
             >
@@ -43,49 +65,69 @@ export default function Event(props) {
             </Link>
           </Typography>
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <strong>{thisEvent.date}</strong> : {start_time} - {end_time}
-          </Typography>
+        <Grid item xs={2}>
+          <Box component="nav">
+            <Button
+              variant="outlined"
+              color="primary"
+              size="small"
+              onClick={handleAttendClick}
+            >
+              <CheckCircleOutlineOutlined className={classes.attendence} />
+              Attend
+            </Button>
+            <Button variant="outlined" color="primary" size="small">
+              <ExposurePlus1 className={classes.attendence} />
+              Invite
+            </Button>
+          </Box>
         </Grid>
       </Grid>
       <Grid container className="theme-paddingx2" spacing={1}>
         <Grid item xs={12} sm={8}>
           <Box>
-            <Typography variant="h6">Location:</Typography>
-            <GoogleMap location={thisEvent.location} />
-            {thisEvent.location}
-          </Box>
-          <Box>
             <Typography variant="h6">About:</Typography>
             <Typography variant="body1">{thisEvent.description}</Typography>
           </Box>
+          <br />
+          <Divider />
+          <Box>
+            <Typography variant="h6">Location:</Typography>
+            <Typography variant="body2">{thisEvent.location}</Typography>
+            <br />
+            <GoogleMap location={thisEvent.location} />
+          </Box>
         </Grid>
-        {
-          thisEvent.members.length ?
-            <Grid item xs={12} sm={4}>
-              <Typography variant="h6">Attendees: </Typography>
-              <Divider />
-              <List>
-                {
-                  thisEvent.members.map(member => (
-                    <ListItem>
-                      <ListItemAvatar>
-                        <Avatar>
-                          <img src={ExtractProfileImage(member)} alt={member.name} />
-                          <AccountCircle />
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText>
-                        <Link href={`/community/${thisEvent.CommunityId}/friends/${member.id}`}>{member.name}</Link>
-                      </ListItemText>
-                    </ListItem>
-                  ))
-                }
-              </List>
-            </Grid>
-            : ''
-        }
+        {thisEvent.members.length ? (
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6">Attendees: </Typography>
+            <Divider />
+            <List>
+              {thisEvent.members.map(member => (
+                <ListItem key={member.id}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <img
+                        src={ExtractProfileImage(member)}
+                        alt={member.name}
+                      />
+                      <AccountCircle />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText>
+                    <Link
+                      href={`/community/${thisEvent.CommunityId}/friends/${member.id}`}
+                    >
+                      {member.name}
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+        ) : (
+          ""
+        )}
       </Grid>
     </>
   );
