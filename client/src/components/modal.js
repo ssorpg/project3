60627e75c4a552215b9d092b67a96eef1d0743bb
kevdoan <kevdoan91@gmail.com>
2019-09-11 13:12:41 -0700

    kinda works

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -8,17 +8,19 @@ const useStyles = makeStyles(theme => ({
   alert: {
     zIndex: '99',
     minWidth: '150px',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    position: 'fixed',
+    //i gave up on the dialog component and was trying to vanilla CSS it here
+    background: 'rgba(0,0,0,0.5)'
   },
-
   noMB: {
-    marginBottom: 0
-  }
+    marginBottom: 0,
+  },
 }));
 
 export default function Modal(props) {
   const { error, success } = props;
-  
+
   const classes = useStyles();
 
   function dismissAlerts() { // dismiss error messages by clicking on them
@@ -30,23 +32,108 @@ export default function Modal(props) {
   }
 
   return (
-    <div onClick={dismissAlerts} className={classes.alert + " no-margin alert"}>
-      {
-        error ?
-          <div className={classes.noMB + " alert alert-danger"}>
-            <strong>Error: </strong>
-            {error}
-          </div>
-          : ''
-      }
-      {
-        success ?
-          <div className={classes.noMB + " alert alert-success"}>
-            <strong>Success: </strong>
-            {success}
-          </div>
-          : ''
-      }
-    </div>
+      <div onClick={dismissAlerts} className={classes.alert + " no-margin alert"}>
+        {
+          error ?
+            <div className={classes.noMB + " alert alert-danger"}>
+              <strong>Error: </strong>
+              {error}
+            </div>
+            : ''
+        }
+        {
+          success ?
+            <div className={classes.noMB + " alert alert-success"}>
+              <strong>Success: </strong>
+              {success}
+            </div>
+            : ''
+        }
+      </div>
   );
 };
+
+
+// import React from 'react';
+// import {
+//   Button, Dialog, DialogActions,
+//   DialogContent, DialogContentText,
+//   DialogTitle, Slide
+// } from '@material-ui/core';
+
+
+
+// export default function Modal({ error, success }) {
+//   const [open, setOpen] = React.useState(false);
+//   React.useState(false);
+
+//   const Transition = React.forwardRef(function Transition(props, ref) {
+//     return <Slide direction="up" ref={ref} {...props} />;
+//   });
+
+//   function handleOpen() {
+//     setOpen(true);
+//     console.log('is this working');
+//   }
+
+//   function handleClose() {
+//     setOpen(false);
+//     console.log('something');
+//   }
+
+//   return (
+//     <div>
+//       {
+//         error ?
+//           <>
+//             <Dialog
+//               //this line below doesn't work, open is a prop which takes only a boolean
+//               open={open, handleOpen}
+//               TransitionComponent={Transition}
+//               onClose={handleClose}
+//               aria-labelledby="alert-dialog-slide-title"
+//               aria-describedby="alert-dialog-slide-description"
+//             >
+//               <DialogTitle id="alert-dialog-slide-title">{"Error!"}</DialogTitle>
+//               <DialogContent>
+//                 <DialogContentText id="alert-dialog-slide-description">
+//                   {error}
+//                 </DialogContentText>
+//               </DialogContent>
+//               <DialogActions>
+//                 <Button onClick={handleClose} color="primary">
+//                   Got it
+//           </Button>
+//               </DialogActions>
+//             </Dialog>
+//           </>
+//           : ''
+//       }
+//       {
+//         success ?
+//           <>
+//             <Dialog
+//               open={handleOpen}
+//               TransitionComponent={Transition}
+//               onClose={handleClose}
+//               aria-labelledby="alert-dialog-slide-title"
+//               aria-describedby="alert-dialog-slide-description"
+//             >
+//               <DialogTitle id="alert-dialog-slide-title">{"Success!"}</DialogTitle>
+//               <DialogContent>
+//                 <DialogContentText id="alert-dialog-slide-description">
+//                   {success}
+//                 </DialogContentText>
+//               </DialogContent>
+//               <DialogActions>
+//                 <Button onClick={handleClose} color="primary">
+//                   Got it
+//           </Button>
+//               </DialogActions>
+//             </Dialog>
+//           </>
+//           : ''
+//       }
+//     </div>
+//   );
+// }
